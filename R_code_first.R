976072ce3607265e9405a6890a59f1733fa4ec9e
Cristián J Monaco <cristianmonaco@gmail.com>
2022-09-21 05:18:35 -1000

    Update R_code_first.R

--- R_code_first.R
+++ R_code_first.R
@@ -1,8 +1,7 @@
-# First script for the RS course
 # Course at Physalia
 # https://www.physalia-courses.org/courses-workshops/remote-sensing-in-r/
 
-# INTRODUCTION TO ECOLOGICAL REMOTE SENSING IN R
+# INTRODUCTION TO ECOLOGICAL REMOTE SENSING IN R ####
 
 # Libraries needed:
 # raster
@@ -16,13 +15,13 @@
 
 # How to install them, e.g. just type the following command in the R console:
 
-install.packages("raster")
+# install.packages("raster")
 
 # then choose the CRAN, i.e. the city from the Comprehensive R Archive Network (any of them is ok!)
 
 # To check if the package is in, simply type:
 
-library(raster)
+# library(raster)
 
 # If nothing happens, that's ok! If you receive an error, write to:
 # Elisa Thouverai <elisa.thouverai2@unibo.it>
@@ -37,13 +36,14 @@ library(raster)
 library(raster)
 
 # Set the working directory
-setwd("~/lab/") # Linux 
+# setwd("~/lab/") # Linux 
 # setwd("C:/lab/")  # windows
 # setwd("/Users/name/lab/") # mac
 
 # We are going to import satellite data
 # objects cannot be numbers
-l2011 <- brick("p224r63_2011.grd")
+# p stands for path and r is row
+l2011 <- brick('lab/data_book/data_book/raster_data/final/p224r63_2011.grd')
 
 l2011
 
@@ -52,18 +52,40 @@ l2011
 # B1 is the reflectance in the blue band
 # B2 is the reflectance in the green band
 # B3 is the reflectance in the red band
+# B4 is the reflectance in the NIR band
 
-cl <- colorRampPalette(c("black","grey","light grey"))(100)
-plot(l2011, col=cl)
 
-plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+# Importing past data
+l1988 <- brick("lab/data_book/data_book/raster_data/final/p224r63_1988.grd")  
+l1988
 
-#----------------- day 2
+par(mfrow=c(2,1))
+plotRGB(l1988, r=4, g=3, b=2, stretch="Hist")  
+plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")  
+
+# Put the NIR in the red channel
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")  
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")  
+
+
+#----------------- day 2 ####
 
 # B1 is the reflectance in the blue band
 # B2 is the reflectance in the green band
 # B3 is the reflectance in the red band
 # B4 is the reflectance in the NIR band
+# B5 swir
+# B6 is the thermal IR (resolution 60 m)
+# B7 swir
+
+cl <- colorRampPalette(c("black","grey","light grey"))(100) # color ramp palette black and gray
+plot(l2011, col = cl)
+
+plot(l2011$B1_sre, col = cl) # blue band
+plot(l2011[[1]], col = cl) # blue band, first element
+
+
+
 
 # let's plot the green band
 plot(l2011$B2_sre)
@@ -90,8 +112,24 @@ par(mfrow=c(2,1))
 plot(l2011$B1_sre, col=clb)
 plot(l2011$B2_sre, col=clg)
 
+dev.off()
 
-#--- day 3
+
+# plotRGB function
+
+plotRGB(l2011, r = 3, g = 2, b = 1, stretch = "Lin") # natural colors, or how we perceive them
+# numbers next to r, g, b are indices of bands. This is the default order.
+# linear stretching allows filling gaps by stretching the data range up to 255 bits 
+# the RGB does not allow working with the shadows. Switch the bands to use the NIR
+
+plotRGB(l2011, r = 4, g = 3, b = 2, stretch = "Lin") # NIR band placed in red (false colours)
+plotRGB(l2011, r = 3, g = 4, b = 2, stretch = "Lin") # NIR band placed in green (false colours)
+
+plotRGB(l2011, r = 4, g = 3, b = 2, stretch = "Hist") # histogram stretching for accentuating differences between mean and extreme values (false colours)
+
+
+
+#--- day 3 ####
 
 plot(l2011$B1_sre)
 
@@ -153,16 +191,3 @@ plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")  # false colours
 
 plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")  
 plotRGB(l2011, r=4, g=3, b=2, stretch="Hist") 
-
-# Importing past data
-setwd("~/lab/") # in case you are on a new R session
-l1988 <- brick("p224r63_1988.grd")  
-l1988
-
-par(mfrow=c(2,1))
-plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")  
-plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")  
-
-# Put the NIR in the blue channel
-plotRGB(l1988, r=2, g=3, b=4, stretch="Lin")  
-plotRGB(l2011, r=2, g=3, b=4, stretch="Lin")  
